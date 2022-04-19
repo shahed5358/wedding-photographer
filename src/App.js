38fb44e6aa0b001e1bc7b608eafd344b393b4545
Mohammad Shahed <mohammadshahed5358@gmail.com>
2022-04-19 17:41:11 +0600

    signOut button handle

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import About from './Components/About/About';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
 import Login from './Components/Login/Login';
-import Services from './Components/Services/Services';
 import Footer from './Components/Footer/Footer';
 import Blogs from './Components/Blogs/Blogs';
 import Contact from './Components/Contact/Contact';
 import SignUp from './Components/SignUp/SignUp';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -18,9 +18,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-        <Route path="/services" element={<Services></Services>}></Route>
-        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={
+          <RequireAuth>
+            <About></About>
+          </RequireAuth>
+        }></Route>
+        <Route path="/blogs" element={
+          <RequireAuth>
+            <Blogs></Blogs>
+          </RequireAuth>
+        }></Route>
         <Route path="/contact" element={<Contact></Contact>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signup" element={<SignUp></SignUp>}></Route>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,16 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Header.css'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <div className="header">
             <nav>
@@ -11,7 +19,12 @@ const Header = () => {
                 <Link to="/blogs">Blogs</Link>
                 <Link to="/contact">Contact</Link>
                 <Link to="/about">About</Link>
-                <Link to="/login">Login</Link>
+                {
+                    user ? 
+                    <button onClick={handleSignOut}>Sign Out</button>
+                    : 
+                    <Link to="/login">Login</Link>
+                }
             </nav>
         </div>
     );
